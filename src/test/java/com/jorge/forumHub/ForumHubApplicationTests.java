c656c2034169a81a9a9d6a10d7ae3a500885034e
Jorgel1 <jorgegothicmetal@gamail.com>
2024-06-10 18:08:35 -0300

    first commit

---
+++ src/test/java/com/jorge/forumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.forumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
